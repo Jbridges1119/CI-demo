8130923e74b78a7e67bc45c35a46066d9005c24f
Jeff Bridges <jbridges1119@gmail.com>
2022-11-05 11:32:22 -0600

    first commit

---
+++ application/views/bye_world.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1><?php echo 'bye_world'; ?></h1>
+<?php
+//segment 3 is the url segment after bye_world - 2 would be bye_world
+	$data = $this->uri->segment(3);
+ echo $data
+?>
+
+<?php 
+//localhost/foler
+echo base_url();
+//localhost/folder/index.php
+echo site_url();
+
+?>
+	</body>
+</html>
