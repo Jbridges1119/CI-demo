4a6f4c9168e7622c8dc852a7e2dbc7b383320311
Jeff Bridges <jbridges1119@gmail.com>
2022-11-05 17:44:17 -0600

    Add dashboard boilerplate

---
+++ application/views/dash/dash_home.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if ( !$_SESSION['u_name'] ) {
+	redirect('home', 'refresh');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
+  </head>
+  <body>
+
+	<!-- dash nav -->
+	<?php $this->load->view('dash/inc/nav'); ?>
+	
+
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
+  </body>
+</html>
